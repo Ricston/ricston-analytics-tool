ebf041000be9e78164e53ecd5533676f054adde9
Alan Cassar <balinju@gmail.com>
2014-08-29 12:33:19 +0200

    adding shutdown hook to cleanly close the context

--- statistics-collector-service/src/main/java/com/ricston/mule/statistics/main/Main.java
+++ statistics-collector-service/src/main/java/com/ricston/mule/statistics/main/Main.java
@@ -3,6 +3,7 @@ package com.ricston.mule.statistics.main;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.ricston.mule.jmx.Connector;
@@ -12,10 +13,17 @@ public class Main {
 	static protected Log logger = LogFactory.getLog(Main.class);
 	
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+		final ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
 		Connector jmxConnector = context.getBean(Connector.class);
 		logger.info("Collector started. " + jmxConnector.toString());
-		//((ConfigurableApplicationContext)context).close();
+		
+		//add shutdown hook to close the Spring context cleanly
+		Runtime.getRuntime().addShutdownHook(new Thread(){
+			@Override
+			public void run() {
+				((ConfigurableApplicationContext)context).close();
+			}
+		});
 	}
 
 }

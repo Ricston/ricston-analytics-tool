7b9e3df16aeb8294222a3c477ebef40f3d3a3016
Alan Cassar <balinju@gmail.com>
2014-10-01 16:48:05 +0200

    ignorning java doc directory

--- statistics-collector-service/src/main/java/com/ricston/mule/statistics/main/ElasticSearchUpload.java
+++ statistics-collector-service/src/main/java/com/ricston/mule/statistics/main/ElasticSearchUpload.java
@@ -23,30 +23,74 @@ import com.ricston.mule.statistics.json.StatisticsToJson;
 import com.ricston.mule.statistics.model.AbstractCollectorStatistics;
 import com.ricston.mule.statistics.model.StatisticsType;
 
+/**
+ * Upload data to Elastic Search. Both single and bulk upload methods 
+ * are available
+ *
+ */
 @Component
 public class ElasticSearchUpload {
 	
+	/**
+	 * The Elastic Search host
+	 */
 	@Value("${elasticsearch.host}")
 	private String host;
 	
+	/**
+	 * The Elastic Search port
+	 */
 	@Value("${elasticsearch.port}")
 	private Integer port;
 	
+	/**
+	 * The transformer to convert statistics into JSON notation (auto wired)
+	 */
 	@Autowired
 	private StatisticsToJson statisitcsToJson;
 	
+	/**
+	 * Date format to use for the index name
+	 */
 	private final DateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd");
 	
+	/**
+	 * Elastic Search API URL template with place holders for host, port, date and type
+	 */
 	private static final String ELASTIC_SEARCH_API_URI = "http://%s:%d/mule-%s/%s";
+	
+	/**
+	 * Elastic Search BULK API URL template with place holders for host, port, date and type
+	 */
 	private static final String ELASTIC_SEARCH_BULK_API_URI = ELASTIC_SEARCH_API_URI + "/_bulk";
 	
+	/**
+	 * Elastic Search BULK API create command
+	 */
 	private static final String ELASTIC_SEARCH_CREATE_BULK = "{ \"create\" : { } }\n";
+	
+	/**
+	 * New Line
+	 */
 	private static final String NEW_LINE = "\n";
 	
+	/**
+	 * Logger
+	 */
 	private Log logger = LogFactory.getLog(getClass());
 	
+	/**
+	 * HTTP Client
+	 */
 	private CloseableHttpClient httpClient = HttpClients.createDefault();
 	
+	/**
+	 * Upload single statistic to Elastic Search
+	 * 
+	 * @param stat The statistic to be uploaded
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 */
 	public void uploadData(AbstractCollectorStatistics stat) throws ClientProtocolException, IOException{
 		
 		logger.debug("Uploading single data item to Elastic Search");
@@ -55,8 +99,10 @@ public class ElasticSearchUpload {
 		
 		try
 		{
+			//convert to JSON
 			String json = statisitcsToJson.toJson(stat);
 			
+			//create and execute HTTP POST
 			HttpPost post = new HttpPost(String.format(ELASTIC_SEARCH_API_URI, host, port, dateFormat.format(new Date()), stat.getType().toString()));
 			StringEntity entity = new StringEntity(json);
 			post.setEntity(entity);
@@ -74,6 +120,9 @@ public class ElasticSearchUpload {
 		logger.debug("Upload complete");
 	}
 	
+	/**
+	 * Upload a list of statistics to Elastic Search using the bulk upload API
+	 */
 	public void uploadBulkData(StatisticsType type, List<? extends AbstractCollectorStatistics> stats) throws ClientProtocolException, IOException{
 		
 		logger.debug("Uploading bulk items to Elastic Search");
@@ -82,16 +131,23 @@ public class ElasticSearchUpload {
 		
 		try
 		{
+			//convert the list of statistics into a bulk request
 			StringBuilder builder = new StringBuilder();
 			
+			//for each statistic, create a line with the create command
+			//followed by the statistic itself
 			for (AbstractCollectorStatistics stat : stats){
 				
+				//convert to JSON
 				String json = statisitcsToJson.toJson(stat);
 				
+				//append create command
 				builder.append(ELASTIC_SEARCH_CREATE_BULK);
+				//append json representation of statistic
 				builder.append(json + NEW_LINE);
 			}
 			
+			//create and execute HTTP POST
 			HttpPost post = new HttpPost(String.format(ELASTIC_SEARCH_BULK_API_URI, host, port, dateFormat.format(new Date()), type.toString()));
 			StringEntity entity = new StringEntity(builder.toString());
 			post.setEntity(entity);
@@ -109,26 +165,50 @@ public class ElasticSearchUpload {
 		logger.debug("Upload complete");
 	}
 
+	/**
+	 * 
+	 * @return The Elastic Search host
+	 */
 	public String getHost() {
 		return host;
 	}
 
+	/**
+	 * 
+	 * @param host The Elastic Search host
+	 */
 	public void setHost(String host) {
 		this.host = host;
 	}
 
+	/**
+	 * 
+	 * @return The Elastic Search port
+	 */
 	public Integer getPort() {
 		return port;
 	}
-
+	
+	/**
+	 * 
+	 * @param port The Elastic Search port
+	 */
 	public void setPort(Integer port) {
 		this.port = port;
 	}
 
+	/**
+	 * 
+	 * @return The Statistics to JSON transformer
+	 */
 	public StatisticsToJson getStatisitcsToJson() {
 		return statisitcsToJson;
 	}
 
+	/**
+	 * 
+	 * @param statisitcsToJson The Statistics to JSON transformer
+	 */
 	public void setStatisitcsToJson(StatisticsToJson statisitcsToJson) {
 		this.statisitcsToJson = statisitcsToJson;
 	}
--- statistics-collector-service/src/main/java/com/ricston/mule/statistics/main/Main.java
+++ statistics-collector-service/src/main/java/com/ricston/mule/statistics/main/Main.java
@@ -8,11 +8,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.ricston.mule.jmx.Connector;
 
+/**
+ * Main class which starts the Spring Context and adds
+ * a shutdown hook to close the context
+ *
+ */
 public class Main {
 	
 	static protected Log logger = LogFactory.getLog(Main.class);
 	
 	public static void main(String[] args) {
+		
+		//start the Spring context
 		final ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
 		Connector jmxConnector = context.getBean(Connector.class);
 		logger.info("Collector started. " + jmxConnector.toString());

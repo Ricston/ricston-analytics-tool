e12315a5bb82fa94471a7b3b5a4de1ed8d9731a0
Alan Cassar <balinju@gmail.com>
2014-10-03 16:26:14 +0200

    adding test case for collect and upload

---
+++ statistics-collector-service/src/test/java/com/ricston/statistics/schedule/CollectAndUploadTestCase.java
@@ -0,0 +1,56 @@
+package com.ricston.statistics.schedule;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+
+import java.io.IOException;
+
+import javax.management.AttributeNotFoundException;
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanException;
+import javax.management.MalformedObjectNameException;
+import javax.management.ReflectionException;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring-context.xml" })
+public class CollectAndUploadTestCase {
+	
+	@Autowired
+	private CollectAndUpload collectAndUpload;
+	
+	@Rule
+	public WireMockRule wireMockRule = new WireMockRule(9200);
+	
+	@Test
+	public void testCollectAndUpload() throws MalformedObjectNameException, AttributeNotFoundException, InstanceNotFoundException, MBeanException, ReflectionException, IOException{
+		
+		//create stub
+		stubFor(post(urlMatching("/mule-[0-9.]+/.*/_bulk"))
+				.willReturn(aResponse()
+						.withStatus(200)
+						.withBody("{ \"error\" : \"false\" }"))
+				);
+		
+		//collect and upload data
+		collectAndUpload.collectAndUpload();
+		
+		
+		//verify that 8 requests to Elastic Search bulk create were made
+		verify(8, postRequestedFor(urlMatching("/mule-[0-9.]+/.*/_bulk")));
+		
+	}
+
+}
